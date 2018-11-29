e081b32032af4c5017d052613e17016b84cc8fb7
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-11-29 14:19:26 -0500

    update

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
-'use strict'
-const Commands = class {
+'use strict';
+
+exports = module.exports = class HephaestusCommands {
   constructor (program) {
-    this.version = '2.0.1'
-    this.name = 'agartha-cli-commands'
+    const { agartha } = require('hephaestus');
+    const pkg = require('./package.json');
+    this.version = pkg.version;
+    this.name = pkg.name;
     try {
-      const agartha = process.agartha
-      const defaultCommands = agartha.path.join(__dirname, 'lib/commands')
-      const isProject = agartha.isProject()
-      const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands')
-      let relicCommands = null      
-      if (isProject) {
-        relicCommands = agartha.relicDir()
-      }
-      agartha._.each([defaultCommands, relicCommands, localCommands], (path) => {
+      const defaultCommands = agartha.path.join(__dirname, 'lib/commands');
+      const isProject = agartha.isProject();
+      const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands');
+      agartha._.each([defaultCommands, localCommands], (path) => {
         if (agartha.exists(path)) {
             agartha._.each(agartha.readdirSync(path), (directory) => {
-            const exists = agartha.path.join(path, directory, 'index.js')
+            const exists = agartha.path.join(path, directory, 'index.js');
             if (agartha.exists(exists)) {
-              const SubCommand = require(exists)
-              const subcommand = new SubCommand()
+              const SubCommand = require(exists);
+              const subcommand = new SubCommand();
               subcommand.options.forEach((option) => {
-                program.option(option.flag, option.description)
+                program.option(option.flag, option.description);
               })
               if (agartha._.isBoolean(subcommand.onDone)) {
                 if (subcommand.onDone) {
-                  process.agartha._onDone.push({ action: subcommand.action, command: subcommand.command})
+                  process.agartha._onDone.push({ action: subcommand.action, command: subcommand.command});
                 }
               }
               if (agartha._.isBoolean(subcommand.onForge)) {
                 if (subcommand.onForge) {
-                  process.agartha._onForge.push({ action: subcommand.action, command: subcommand.command})
+                  process.agartha._onForge.push({ action: subcommand.action, command: subcommand.command});
                 }
               }              
               program.command(subcommand.command)
                 .description(subcommand.description)
                 .alias(subcommand.alias)
-                .action(subcommand.action)
+                .action(subcommand.action);
               }
             })
           }
       })
-      program.parse(process.argv)    
-    } catch (e) {
-      console.error('outer', e.message)
+      program.parse(process.argv);
+    }
+    catch (e) {
+      console.error('outer', e.message);
     }
   }
 }
-
-exports = module.exports = Commands
