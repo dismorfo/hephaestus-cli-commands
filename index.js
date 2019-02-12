ab200084c17c924feab7bafa748fc451e3493da7
Alberto Ortiz Flores <dismorfo@gmail.com>
2019-02-12 15:11:24 -0500

    update

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 'use strict';
 
-exports = module.exports = class HephaestusCommands {
+class HephaestusCommands {
   constructor (program) {
-    const agartha = require('hephaestus');
+    const { appDir, exists, readdirSync } = require('hephaestus');
     const pkg = require('./package.json');
+    const { join } = require('path');
+    const _ = require('underscore');
     this.version = pkg.version;
     this.name = pkg.name;
     try {
-      const defaultCommands = agartha.path.join(__dirname, 'lib/commands');
-      const isProject = agartha.isProject();
-      const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands');
-      agartha._.each([defaultCommands, localCommands], (path) => {
-        if (agartha.exists(path)) {
-            agartha._.each(agartha.readdirSync(path), (directory) => {
-            const exists = agartha.path.join(path, directory, 'index.js');
-            if (agartha.exists(exists)) {
-              const SubCommand = require(exists);
+      const defaultCommands = join(__dirname, 'lib/commands');
+      const localCommands = join(appDir(), 'app', 'commands');
+      _.each([defaultCommands, localCommands], (path) => {
+        if (exists(path)) {
+            _.each(readdirSync(path), (directory) => {
+              const commandsExists = join(path, directory, 'index.js');
+            if (exists(commandsExists)) {
+              const SubCommand = require(commandsExists);
               const subcommand = new SubCommand();
               subcommand.options.forEach((option) => {
                 program.option(option.flag, option.description);
               });
-              if (agartha._.isBoolean(subcommand.onDone)) {
-                if (subcommand.onDone) {
-                  process.agartha._onDone.push({ action: subcommand.action, command: subcommand.command});
-                }
-              }
-              if (agartha._.isBoolean(subcommand.onForge)) {
-                if (subcommand.onForge) {
-                  process.agartha._onForge.push({ action: subcommand.action, command: subcommand.command});
-                }
-              }
+
+              // if (_.isBoolean(subcommand.onDone)) {
+                // if (subcommand.onDone) {
+                  // process.agartha._onDone.push({ action: subcommand.action, command: subcommand.command});
+                //}
+              //}
+
+              //if (_.isBoolean(subcommand.onForge)) {
+                 // if (subcommand.onForge) {
+                  // process.agartha._onForge.push({ action: subcommand.action, command: subcommand.command});
+                // }
+              //}
               // register command
               // .list = true or command was passed as argument
-              if (subcommand.list || (!subcommand.list && !agartha._.isUndefined(process.argv[2]) && process.argv[2] == subcommand.command)) {
+
+              if (subcommand.list || (!subcommand.list && !_.isUndefined(process.argv[2]) && process.argv[2] == subcommand.command)) {
                 program.command(subcommand.command)
                   .description(subcommand.description)
                   .alias(subcommand.alias)
@@ -45,7 +49,9 @@ exports = module.exports = class HephaestusCommands {
       program.parse(process.argv);
     }
     catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
 }
+
+exports = module.exports = HephaestusCommands;
